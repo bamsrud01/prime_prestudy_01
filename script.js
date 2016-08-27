58ca1da906992075d5f27d16ca49319edf922ace
Barrett Amsrud <barrettamsrud@gmail.com>
2016-08-27 08:02:45 +0000

    Complete week 3 bonus

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function working() {
 };
 
 working();
+
+function addSeven(num) {
+    return num + 7;
+}
+
+function subtractTwo(num) {
+    return num - 2;
+}
+
+function multiplyEight(num) {
+    return num * 8;
+}
+
+console.log(multiplyEight(addSeven(subtractTwo(3))))
